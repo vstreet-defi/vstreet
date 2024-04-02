faa0cbcd0f361599c7c7224649ce8a0c2fb3e50e
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-04-02 09:30:41 -0600

    Update Hero.tsx

--- frontend/src/components/organisms/Hero/Hero.tsx
+++ frontend/src/components/organisms/Hero/Hero.tsx
@@ -6,13 +6,13 @@ import {
   Text,
   Button,
   Image,
+  Heading,
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
-import Gusano from "../../../assets/images/3d Swirl Shape.svg";
 import LogoWhite from "../../../assets/images/vara street logoHOMEWhite.svg";
 import Home1VideoBg from "../../../assets/images/backgrounds/Home-Gif-128colorsWebComp1728-12fps.gif";
-
+import Home1VideoBgSmall from "../../../assets/images/backgrounds/Home-Gif-WebComp1280x741-12fps.gif";
 function Hero() {
   const navigate = useNavigate();
 
@@ -20,11 +20,11 @@ function Hero() {
     <Container p="0" maxW="100vw">
       <Box
         w="100vw"
-        h={{ base: "100vh", md: "80vh" }}
+        h={{ base: "100vh", md: "100vh" }}
         bgRepeat="no-repeat"
         bgPos="bottom"
         bgSize={{ base: "cover", md: "fill" }}
-        bgImage={Home1VideoBg}
+        bgImage={{ base: Home1VideoBgSmall, md: Home1VideoBg }}
       >
         <Stack
           pl={{ base: "1rem", md: "3rem", xl: "8rem", "2xl": "15rem" }}
@@ -35,12 +35,21 @@ function Hero() {
           direction={{ base: "column", md: "row" }}
         >
           <Stack flex={1} spacing={{ base: 5, md: 10 }}>
-            <Image w="30rem" src={LogoWhite} />
+            <Heading
+              bgGradient="linear(to-b, #00FFC4, #4FFF4B)"
+              bgClip="text"
+              fontSize={{ base: "4rem", sm: "5rem", lg: "128px" }}
+              fontWeight="extrabold"
+              fontFamily={"'Roboto Mono', monospace"}
+            >
+              VARA <br></br> DEFI-HUB
+            </Heading>
 
             <Text
               color="gray.100"
               fontWeight="light"
-              fontSize={{ base: "1xl", sm: "3xl", lg: "4xl" }}
+              fontSize={{ base: "1rem", sm: "1rem", lg: "24px" }}
+              fontFamily={"'Roboto Mono', monospace"}
             >
               Unlock the synthetic assets liquidity to optimize your DeFi
               strategies
