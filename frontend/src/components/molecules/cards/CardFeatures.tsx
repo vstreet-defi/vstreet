2b8725fe42934c05efd8078cd18f5a107e2aaee3
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-04-04 15:05:32 -0600

    Services or Features Section Implementation 1

--- frontend/src/components/molecules/cards/CardFeatures.tsx
+++ frontend/src/components/molecules/cards/CardFeatures.tsx
@@ -1,28 +1,37 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Container, Flex, Text } from "@chakra-ui/react";
 import styles from "./Card.module.scss";
 
-function CardFeatures(props: { title: string }) {
+function CardFeatures(props: { title: string; w: string }) {
   return (
-    <Box
-      m={{ base: ".5rem", sm: "1rem", md: "2rem" }}
-      borderWidth="1px"
-      pt="1rem"
-      pb="1rem"
-      pl={"2rem"}
-      pr={"2rem"}
-      className={styles.CardFeature}
-      w="8rem"
-      style={{
-        boxShadow: "0px 14px 25.4px -3px rgba(59, 255, 106, 0.52)",
-        backdropFilter: "blur(5.449999809265137px)",
-      }}
-    >
-      <Heading fontSize="12px" color="white">
-        {props.title}
-      </Heading>
-    </Box>
+    <Flex flexDir="column">
+      <Box
+        m={{ base: ".5rem", sm: "1rem", md: "2rem" }}
+        borderWidth="1px"
+        pt="1rem"
+        pb="1rem"
+        pl={"2rem"}
+        pr={"2rem"}
+        className={styles.CardFeature}
+        w={props.w}
+        style={{
+          boxShadow: "0px 14px 25.4px -3px rgba(59, 255, 106, 0.52)",
+          backdropFilter: "blur(5.449999809265137px)",
+        }}
+        justifyItems="center"
+      >
+        <Text
+          fontSize="12px"
+          color="#00FFC4"
+          fontFamily={"'Roboto Mono', monospace"}
+          alignSelf="center"
+          justifyContent={"center"}
+        >
+          {props.title}
+        </Text>
+      </Box>
+    </Flex>
   );
 }
 
--- frontend/src/components/organisms/Features/Features.tsx
+++ frontend/src/components/organisms/Features/Features.tsx
@@ -1,11 +1,18 @@
 import { Container, Flex, Box, Heading, Text, Stack } from "@chakra-ui/react";
 import { CardFeatures } from "components/molecules/cards/CardFeatures";
+import { Arrow } from "components/atoms/Arrow";
 import bg from "../../../assets/images/backgrounds/Services BG 2x.svg";
 
 function Features() {
   return (
     <Container p="0" maxW="100vw" bgGradient="linear(to-l, #121215 ,#284736)">
-      <Box w="100vw" bgImg={bg} bgRepeat="no-repeat" bgPos="center">
+      <Box
+        w="100vw"
+        bgImg={bg}
+        bgRepeat="no-repeat"
+        bgPos="center"
+        bgSize="cover"
+      >
         <Stack
           pl={{ base: "1rem", md: "3rem", xl: "8rem", "2xl": "15rem" }}
           pr={{ base: "1rem" }}
@@ -15,13 +22,17 @@ function Features() {
           direction={{ base: "column", md: "row" }}
         >
           <Stack flex={1} spacing={{ base: 5, md: 10 }}>
-            <Stack spacing={{ base: 4, sm: 6 }} direction="column">
-              <CardFeatures title="Stake Vara" />
-              <CardFeatures title="LST $Vara as Collateral" />
-              <CardFeatures title="Borrow Stable Coins" />
-              <CardFeatures title="Stake Vara" />
-            </Stack>
+            <Flex flexDir="column" alignItems="center">
+              <CardFeatures title="Stake Vara" w="9rem" />
+              <Arrow />
+              <CardFeatures title="LST $Vara as Collateral" w="15rem" />
+              <Arrow />
+              <CardFeatures title="Borrow Stable Coins" w="13rem" />
+              <Arrow />
+              <CardFeatures title="DeFi Strategie Legos" w="12rem" />
+            </Flex>
           </Stack>
+
           <Flex
             flex={1}
             justify="center"
@@ -33,14 +44,23 @@ function Features() {
             <Heading
               bgGradient="linear(to-b, #00FFC4, #4FFF4B)"
               bgClip="text"
-              fontSize={{ base: "4rem", sm: "5rem", lg: "48px" }}
+              fontSize={{ base: "3rem", sm: "5rem", lg: "48px" }}
               fontWeight="extrabold"
               fontFamily={"'Roboto Mono', monospace"}
             >
               Make your staked $VARA liquid
             </Heading>
-            <Text color="#ffffff" mr="2rem" mt="2rem" fontSize="24px">
+            <Text
+              color="#ffffff"
+              mr={{ md: "2rem", lg: "4rem" }}
+              mt="2rem"
+              fontSize={{ md: "12px", lg: "24px" }}
+              fontFamily={"'Roboto Mono', monospace"}
+              fontWeight="thin"
+              textAlign="justify"
+            >
               Leverage your staked $VARA as collateral for stablecoin borrowing.
+              <br /> <br></br>
               Our streamlined platform offers secure, transparent lending,
               giving you the flexibility to enhance your investment strategies
               effectively.
