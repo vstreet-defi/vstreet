358acc3f7069ed5ef827696e79d74f289c5d10f3
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-06-03 11:47:55 -0600

    firs loadingSpinner implementation

--- frontend/src/components/atoms/Button-Gradient-Fill/ButtonGradFill.tsx
+++ frontend/src/components/atoms/Button-Gradient-Fill/ButtonGradFill.tsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
+
 interface ButtonProps {
   label: string;
 }
 
 function ButtonGradFill({ label }: ButtonProps) {
+  const [isLoading, setLoading] = useState(false);
   const handleClick = () => {
+    setLoading(true);
     if (label === "Deposit") console.log("Deposit action performed");
     if (label === "Withdraw") console.log("Withdraw action performed");
   };
 
   return (
-    <button className="btn-grad-fill" onClick={handleClick}>
-      {label}
+    <button
+      className={`btn-grad-fill ${isLoading ? "btn-grad-fill--loading" : ""}`}
+      onClick={handleClick}
+    >
+      {isLoading ? (
+        <div className="lds-facebook">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      ) : (
+        label
+      )}
     </button>
   );
 }
