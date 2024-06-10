5e8a542285e970f489399994f4fea71a564cd2bd
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-06-10 15:46:11 -0600

    [Feature][VST-49]: Implement Spinner (#20)
    * firs loadingSpinner implementation
    
    * master merge

--- frontend/src/components/atoms/Button-Gradient-Fill/ButtonGradFill.tsx
+++ frontend/src/components/atoms/Button-Gradient-Fill/ButtonGradFill.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AlertModalContext } from "contexts/alertContext";
 
 interface ButtonProps {
@@ -6,10 +6,12 @@ interface ButtonProps {
 }
 
 const ButtonGradFill: React.FC<ButtonProps> = ({ label }) => {
+  const [isLoading, setIsLoading] = useState(false);
   const alertModalContext = useContext(AlertModalContext);
   const handleClick = () => {
     alertModalContext?.showAlertModal("Button clicked");
     if (alertModalContext) {
+      setIsLoading(true);
       if (label === "Deposit") console.log("Deposit action performed");
       if (label === "Withdraw") console.log("Withdraw action performed");
       console.log(
@@ -20,8 +22,19 @@ const ButtonGradFill: React.FC<ButtonProps> = ({ label }) => {
   };
 
   return (
-    <button className="btn-grad-fill" onClick={handleClick}>
-      {label}
+    <button
+      className={`btn-grad-fill ${isLoading ? "btn-grad-fill--loading" : ""}`}
+      onClick={handleClick}
+    >
+      {isLoading ? (
+        <div className="lds-facebook">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      ) : (
+        label
+      )}
     </button>
   );
 };
