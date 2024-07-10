1cfe2d4222e331589b94c1343fe8d319304d3456
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-07-10 13:17:02 -0600

    read state

--- frontend/src/components/pages/Dapp.tsx
+++ frontend/src/components/pages/Dapp.tsx
@@ -29,7 +29,7 @@ function DappPage() {
               bannerComponent={
                 <>
                   <AlertModal type="warning" />
-                  <ReadState />
+                  {/* <ReadState /> */}
                 </>
               }
               leftSectionComponent={
