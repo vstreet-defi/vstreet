c4067dfb8122b8ca20d8c5d5570d5e98e4f65bf1
Luchex <lucianog2000@gmail.com>
2024-08-03 20:25:50 -0300

    [TASK][VST-109]: Refactoring and updating Header

--- frontend/src/components/pages/Dapp.tsx
+++ frontend/src/components/pages/Dapp.tsx
@@ -1,23 +1,24 @@
 import DappTemplate from "components/templates/Dapp";
 import ApiLoader from "components/atoms/ApiLoader";
 import { useApi, useAccount } from "@gear-js/react-hooks";
-import Header from "components/templates/Header/Header";
+import Header, { DappTab } from "components/templates/Header/Header";
 import { isMobileDevice } from "utils/isMobile";
 import { AlertModal } from "components/molecules/alert-modal/AlertModal";
 import { FundsManager } from "components/organisms/FundsManager/FundsManager";
 import TotalLiquidityPool from "components/atoms/TotalLiquidityPool/TotalLiquidityPool";
-import { AlertModalContext, AlertModalProvider } from "contexts/alertContext";
-import { useContext, useEffect } from "react";
-import StakingInfoHeader from "components/atoms/Staking-Info-Header/StakingInfoHeader";
-import StakingInfoCard from "components/molecules/Staking-Info-Card/StakingInfoCard";
-import { ReadState } from "components/gear/ReadState";
+import { AlertModalProvider } from "contexts/alertContext";
 import StakingInfo from "components/organisms/StakingInfo/StakingInfo";
 
 function DappPage() {
   const { isApiReady } = useApi();
   const { isAccountReady } = useAccount();
   const isAppReady = isApiReady && isAccountReady;
-  const navBarItems = ["Home", "Supply", "Borrow", "Markets"];
+  const navBarItems = [
+    DappTab.Home,
+    DappTab.Supply,
+    DappTab.Borrow,
+    DappTab.Markets,
+  ];
 
   return (
     <>
